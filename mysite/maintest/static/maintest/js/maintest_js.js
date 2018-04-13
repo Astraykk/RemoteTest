a4fcc2d59d9fa3994a880702fb2b2953ccce53c2
kcl <2097088648@qq.com>
2018-04-13 17:10:45 +0800

    4/13

--- mysite/maintest/static/maintest/js/maintest_js.js
+++ mysite/maintest/static/maintest/js/maintest_js.js
@@ -29,3 +29,27 @@ xmlhttp.onreadystatechange=function()
 xmlhttp.open("GET","{% url 'maintest:arith_result' %}",true);
 xmlhttp.send();
 }
+
+
+function loadXMLDocG()
+{
+var xmlhttp;
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("myAjax").innerHTML=xmlhttp.responseText;
+    }
+  }
+//xmlhttp.open("GET","http://127.0.0.1:8000/maintest/arith_result/",true);
+xmlhttp.open("GET","{% url 'maintest:arith_result' %}",true);
+xmlhttp.send();
+}

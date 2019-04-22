08c3f247f838e2cdb01fdaa145e358e7e185bf92
hxy123abc <34414926+hxy123abc@users.noreply.github.com>
2019-04-22 07:54:43 -0700

    Add files via upload

--- mysite/maintest/views.py
+++ mysite/maintest/views.py
@@ -12,8 +12,7 @@ from filebrowser.base import FileListing, FileObject
 import os, json, re
 from .mytools.patternGen import PatternGen
 from .mytools.mytools import VcdFile, vcd_merge
-
-import time
+from .vcd2pic_views import vcd2picjson
 
 # patternGen.prepare()
 
@@ -90,7 +89,6 @@ def treeview_parser(root='', abspath='', relpath='', flag='C'):
 	for item in filelisting.listing():
 		fileobject = FileObject(os.path.join(path, item))
 		newabspath = os.path.join(abspath, item)
-		# print(newabspath)
 		if flag == 'O':
 			dataList.append({
 				"text": item,
@@ -192,16 +190,7 @@ class MainTest(object):
 	def __init__(self, storage=default_storage):
 		self.storage = storage
 		self.directory = DIRECTORY
-		self.stream_status = [
-			["Check", UNDONE],
-			["Build", UNDONE],
-			["Test", UNDONE],
-			["Report", UNDONE]
-		]  # TODO: extend the item.
-
-	def clr_status(self):
-		for item in self.stream_status:
-			item[1] = UNDONE
+		self.stream_status = [["Check", UNDONE], ["Build", UNDONE], ["Test", UNDONE]]  # TODO: extend the item.
 
 	def file_check(self, request):
 		pass
@@ -211,142 +200,94 @@ class MainTest(object):
 
 	def check(self, request):
 		query = request.GET
-		# self.tfo_path = os.path.join(DIRECTORY, query.get('path', ''))
-		self.tfo_path = self.directory
-		self.tfo_name = query.get('tfo', '')
-		print('path = {}\ntfo = {}'.format(self.tfo_path, self.tfo_name))
-		print('directory =', self.directory)
-		# tfo_file = 'tfo_demo.tfo'
-		# self.pattern = PatternGen(self.tfo_path, self.tfo_name)
-		# TODO: Check file integrity and syntax.
+		path = query.get('path', self.directory)
+		print('path=', path)
+		tfo_file = 'tfo_demo.tfo'
+		self.pattern = PatternGen(path, tfo_file)
 		self.stream_status[0][1] = DONE  # Check status
 		return HttpResponse('check success')
 
 	def build(self, request):
-		from maintest.mytools.batch import batch_build
 		query = request.GET
 		path = query.get('path', '')
-		# print(path)
 		# tfo_file = query.get('tfo', '')
-		# print('path= ', path)
-		# tfo_file = 'tfo_demo.tfo'
-		# self.pattern = PatternGen(path, tfo_file)
+		print('path= ', path)
+		tfo_file = 'tfo_demo.tfo'
+		self.pattern = PatternGen(path, tfo_file)
 		print('initialization success!')
-		# try:
-		# 	# self.pattern.write()
-		# 	batch_build(self.tfo_path, self.tfo_name)
-		# 	print('write success!')
-		# 	self.stream_status[1][1] = DONE  # Build status
-		# 	return HttpResponse("Build Success!")
-		# except Exception as err:
-		# 	return err
-		# self.pattern.write()
-		print('path = {}\ntfo = {}'.format(self.tfo_path, self.tfo_name))
-		batch_build(self.tfo_path, self.tfo_name)
-		print('write success!')
-		self.stream_status[1][1] = DONE  # Build status
-		return HttpResponse("Build Success!")
-
-	# def test(self, request):
-	# 	query = request.GET
-	# 	print(self.directory)
-	# 	path = os.path.join(self.directory)  # query.get('path', ''))
-	# 	# path = query.get('path', '')
-	# 	rpt_name = query.get('rpt_name', 'test_result')
-	# 	i_file = os.path.join(DIRECTORY, path, self.pattern.file_list['BIT']+'.ptn')
-	# 	o_file = os.path.join(DIRECTORY, path, rpt_name + '.trf')
-	# 	vcd_file = os.path.join(DIRECTORY, path, rpt_name + '.vcd')
-	# 	ref_vcd_path = os.path.join(DIRECTORY, path, self.pattern.file_list['VCD'])
-	# 	timescale = self.pattern.digital_param['period']
-	# 	print('i_file = {}\no_file = {}\nvcd_file = {}\n'.format(i_file, o_file, vcd_file))
-	# 	print('wave_path = ' + self.wave_path)
-	# 	try:
-	# 		start_time = time.time()
-	# 		print('Start batch build')
-	# 		msg = os.popen('sudo /home/linaro/BR0101/z7_v4_com/z7_v4_ip_app {} {} 1 1 1'.format(i_file, o_file)).read()
-	# 		# msg = 'test success'
-	# 		print('msg = ', msg)
-	# 		end_time = time.time()
-	# 		print("\nTest time: " + str(end_time - start_time))
-	# 		self.stream_status[2][1] = DONE  # Build status
-	# 		self.pattern.trf2vcd(rpt_name + '.trf', rpt_name + '.vcd', flag='bypass')
-	# 		# from .mytools.vcd2pic.vcd2pic import vcd2pic
-	# 		# pic_path = os.path.join(self.wave_path, self.project_name) + '.jpg'
-	# 		# print(pic_path)
-	# 		# vcd2pic(vcd_file, pic_path)
-	#
-	# 		# vcd merge
-	# 		vcd1 = VcdFile(ref_vcd_path, period=timescale)  # reference vcd
-	# 		# print('vcd1 = ', vcd1.vcd_info)
-	# 		vcd1.get_vcd_info()
-	# 		vcd2 = VcdFile(vcd_file, period='1ps')
-	# 		# print('vcd2 = ', vcd2.vcd_info)
-	# 		vcd2.get_vcd_info()
-	# 		vcdm = vcd_merge(vcd1, vcd2, os.path.join(DIRECTORY, path, rpt_name + '_merge.vcd'))
-	# 		print(vcdm.sym2sig)
-	# 		# vcd2.gen_vcd('pin_test/p1_merge.vcd')
-	# 		vcdm.gen_vcd(vcdm.path)
-	#
-	# 		return HttpResponse(msg)
-	# 	except Exception as err:
-	# 		return HttpResponse(err)
-	# 		# return HttpResponse('error')
+		try:
+			self.pattern.write()
+			print('write success!')
+			self.stream_status[1][1] = DONE  # Build status
+			return HttpResponse("Build Success!")
+		except Exception as err:
+			return err
 
 	def test(self, request):
-		from maintest.mytools.batch import batch_test
 		query = request.GET
-		# print(self.directory)
+		print(self.directory)
 		path = os.path.join(self.directory)  # query.get('path', ''))
+		# path = query.get('path', '')
+		rpt_name = query.get('rpt_name', 'test_result')
+		i_file = os.path.join(DIRECTORY, path, self.pattern.file_list['BIT']+'.ptn')
+		o_file = os.path.join(DIRECTORY, path, rpt_name + '.trf')
+		vcd_file = os.path.join(DIRECTORY, path, rpt_name + '.vcd')
+		ref_vcd_path = os.path.join(DIRECTORY, path, self.pattern.file_list['VCD'])
+		timescale = self.pattern.digital_param['period']
+		print('i_file = {}\no_file = {}\nvcd_file = {}\n'.format(i_file, o_file, vcd_file))
+		print('wave_path = ' + self.wave_path)
 		try:
-			batch_test(self.tfo_path, self.tfo_name)
-			self.stream_status[2][1] = DONE
-			return HttpResponse('Test Success!')
+			msg = os.popen('sudo /home/linaro/BR0101/z7_v4_com/z7_v4_ip_app {} {} 1 1 1'.format(i_file, o_file)).read()
+			#msg = 'test success'
+			print('msg = ', msg)
+			self.stream_status[2][1] = DONE  # Build status
+			self.pattern.trf2vcd(rpt_name+'.trf', rpt_name+'.vcd', flag='bypass')
+			# from .mytools.vcd2pic.vcd2pic import vcd2pic
+			# pic_path = os.path.join(self.wave_path, self.project_name) + '.jpg'
+			# print(pic_path)
+			# vcd2pic(vcd_file, pic_path)
+
+			# vcd merge
+			vcd1 = VcdFile(ref_vcd_path, period=timescale)  # reference vcd
+			# print('vcd1 = ', vcd1.vcd_info)
+			vcd1.get_vcd_info()
+			vcd2 = VcdFile(vcd_file, period='1ps')
+			# print('vcd2 = ', vcd2.vcd_info)
+			vcd2.get_vcd_info()
+			vcdm = vcd_merge(vcd1, vcd2, os.path.join(DIRECTORY, path, rpt_name + '_merge.vcd'))
+			print(vcdm.sym2sig)
+			# vcd2.gen_vcd('pin_test/p1_merge.vcd')
+			vcdm.gen_vcd(vcdm.path)
+
+			return HttpResponse(msg)
 		except Exception as err:
 			return HttpResponse(err)
 			# return HttpResponse('error')
-
-	def report(self, request):
-		from maintest.mytools.batch import batch_trf2vcd, batch_merge
-		try:
-			# print(self.tfo_path, self.tfo_name)
-			batch_trf2vcd(self.tfo_path, self.tfo_name)
-			batch_merge(self.tfo_path, self.tfo_name)
-			self.stream_status[3][1] = DONE
-			return HttpResponse('Report ready!')
-		except Exception as err:
-			return HttpResponse(err)
 
 	def treeview_ajax(self, request):
 		query = request.GET
 		query_dir = query.get('dir', '')
 		query_flag = query.get('flag', '')
 		path = os.path.join(DIRECTORY, query_dir)
-		# if os.path.exists(path):
-		# 	return HttpResponse('Project already exits!')
-		# self.directory = os.path.join(self.directory, query_dir)  # change root directory of the page
-		# self.directory = query_dir  # change root directory of the page
-		self.directory = path  # change root directory of the page
-		# print('ajax_path', self.directory)
-		self.project_name = query_dir
-		self.wave_path = os.path.join(site.storage.location, "maintest/static/maintest/img", query_dir)
-		self.clr_status()
+		#update: do not change root directory of the page here
+		#self.directory = query_dir  # change root directory of the page
+		#self.project_name = query_dir
+		#self.wave_path = os.path.join(site.storage.location, "maintest/static/maintest/img", query_dir)
 		if not os.path.exists(self.wave_path):
 			os.mkdir(self.wave_path)
-		result = treeview_parser(path, flag=query_flag)
+		result = treeview_parser(path, query_flag)
 		return HttpResponse(json.dumps(result), content_type='application/json')
 
 	def edit_file(self, file_path):
 		import binascii
-		# print('directory=', self.directory)
-		# print('file_path=', file_path)
 		if os.path.isfile(file_path):
 			with open(file_path, 'rb+') as f:
 				content = f.read()
-				if os.path.splitext(file_path)[1] == '.ptn':
-					pattern = re.compile('.{32}')
-					content = str(binascii.hexlify(content)).lstrip("b'").upper()
-					content = '\t'.join(re.findall(r'.{4}', content))
-					content = '\n'.join(re.findall(r'.{40}', content))
+				if os.path.splitext(file_path)[1] == '.ptn' or os.path.splitext(file_path)[1] == '.trf':
+					print('ok')
+					content = binascii.hexlify(content)
+					print(len(content))
+					#content=content[0:100]
 			# print(type(content))
 			return content
 		else:
@@ -372,17 +313,20 @@ class MainTest(object):
 		"""
 		query = request.GET
 		query_file = query.get('file', 'open file')
-		# print('query_file(in index)=', query_file)
 		file_path = os.path.join(self.directory, query_file)
-		# print('file_path(in index)=', file_path)
-		# print(query_file, file_path)
 		query_path = query.get('path', '')
-		# self.directory = os.path.join(DIRECTORY, query_path)
-		# print('index_path', self.directory)
+		print('project=', self.directory)
+		#self.prevdirectory = self.directory
+		newdirectory = os.path.join(DIRECTORY, query_path)
+		if self.directory != newdirectory:
+			#entering a new project
+			self.directory= newdirectory
+			#reset stream_status
+			self.stream_status[0][1] = UNDONE
+			self.stream_status[1][1] = UNDONE
+			self.stream_status[2][1] = UNDONE
 		obj = treeview_parser(self.directory, relpath=query_path)
-		# print(obj)
 		tv_dir = treeview_parser(DIRECTORY, flag='O')
-		# print(self.directory)
 		wave_path = os.path.join('maintest/img/', self.directory, '/wave.jpg')
 		return render(request, 'maintest/test.html', {
 			'DIRECTORY': DIRECTORY,
